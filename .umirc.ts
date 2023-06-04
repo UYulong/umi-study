07b39efd93ba5b22b20273a7d7cd7d5456384241
UYulong <1274583083@qq.com>
2023-06-04 18:24:23 +0800

    feat: 完成基本布局

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  plugins: ['@umijs/plugins/dist/react-query'],
+  reactQuery: {},
   routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
+    { path: "/", component: "index", name: 'Home' },
+    { path: "/docs", component: "docs", name: 'Docs' },
+    { path: '/products', component: 'products', name: 'Products' }
   ],
   npmClient: 'pnpm',
 });
---
+++ src/pages/products.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './products.less';
+import { useMutation, useQuery, useQueryClient } from 'umi';
+import axios from 'axios';
+import ProductList from '@/components/ProductList';
+
+export default function Page() {
+  const queryClient = useQueryClient()
+
+  const productQuery = useQuery(['products'], {
+    queryFn() {
+      return axios.get('/api/products').then(res => res.data)
+    }
+  })
+
+  const productsDeleteMutation = useMutation({
+    mutationFn(id: string) {
+      return axios.delete(`/api/products/${id}`)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] })
+    }
+  })
+
+  if (productQuery.isLoading) return null
+
+  return (
+    <div>
+      <h1 className={styles.title}>Page products</h1>
+      <ProductList products={productQuery.data.data} onDelete={(id) => productsDeleteMutation.mutate(id)} />
+    </div>
+  );
+}
